54151a9a3c7ce42cf72d2189149c3d86079634b4
Luke MacLean <40969439+MacLeanLuke@users.noreply.github.com>
2021-06-28 13:47:11 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,34 @@
+var login = document.querySelector("#login")
+var addDefinition = document.querySelector("#add-definition")
+var liked = document.querySelector("#liked")
+
+
+function toggle(element){
+    element.innerHTML = "Logout"
+}
+
+function remove(element){
+    element.remove()
+}
+
+function likey(){
+    alert("Ninja was liked")
+}
+
+
+
+
+
+
+
+
+// function example(element) {
+//     console.log("element clicked", element);
+// }
+
+// function turnOff(element) {
+//     element.innerText = "Off";
+// }
+// function hide(element) {
+//     element.remove();
+// }
